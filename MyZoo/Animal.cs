267dabc807243f7d3efc42b4d4e372237af6d22d
PaulLittle <S00156762@mail.itsligo.ie>
2015-10-20 21:39:03 +0100

    animal class commit

---
+++ MyZoo/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyZoo
+{
+    public enum Gender
+    {
+        M, F
+    }
+    class Animal
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        private Gender _sex;
+        public Gender Sex { get { return _sex; } set { _sex = value; } }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\tSex: {1}", Name, Sex);
+        }
+
+        public Animal() : this("Lassie", Gender.F)
+        {
+
+        }
+
+        public Animal(string nme)
+        {
+            Name = nme;
+        }
+
+        public Animal(string nme, Gender sx)
+        {
+            Name = nme;
+            Sex = sx;
+        }
+
+    }  // end class
+}  // end ns
